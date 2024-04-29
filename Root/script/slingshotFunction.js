366fa14313aa23ca3d451cb72212e146e7fe4144
Richie8y <148615720+Richie8y@users.noreply.github.com>
2024-04-29 12:36:12 +0100

    Added new designs

---
+++ Root/script/slingshotFunction.js
@@ -0,0 +1,15 @@
+function show() {
+    document.getElementById('mini-navpg').classList.toggle('active');
+    document.getElementById('bar1').classList.toggle('active');
+    document.getElementById('bar2').classList.toggle('active');
+    document.getElementById('bar3').classList.toggle('active');
+}
+
+function proj_display() {
+    document.getElementById('proj-page').classList.toggle('active');
+}
+
+function mini_proj_display() {
+    document.getElementById('proj-page').classList.toggle('active');
+    
+}
