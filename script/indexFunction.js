4a4395390d8e37ddda4db6a838efbc738472392d
Richie8y <148615720+Richie8y@users.noreply.github.com>
2024-08-26 21:33:49 +0100

    website rework

--- script/indexFunction.js
+++ script/indexFunction.js
@@ -13,3 +13,11 @@ function mini_proj_display() {
     document.getElementById('proj-page').classList.toggle('active');
     
 }
+
+function mini_proj_display_2(){
+    document.getElementById('proj-page').classList.toggle('active');
+    document.getElementById('mini-navpg').classList.toggle('active');
+    document.getElementById('bar1').classList.toggle('active');
+    document.getElementById('bar2').classList.toggle('active');
+    document.getElementById('bar3').classList.toggle('active');
+}
